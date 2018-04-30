2fc0aaaf01d5a0dfa4a204a2968af7b96f8a3acf
Nicholas Tiner <ntiner1@tamu.edu>
2018-04-30 00:10:57 -0500

    added microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_30_033741) do
+ActiveRecord::Schema.define(version: 2018_04_30_044544) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
