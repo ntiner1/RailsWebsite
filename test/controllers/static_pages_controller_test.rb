31c0835a96c5c3b2a807cc1b88421c75b0ff054f
Nicholas Tiner <ntiner1@tamu.edu>
2018-05-03 23:34:21 -0500

    Updated some of the static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     test "should get root" do
         get root_url
         assert_response :success 
-        assert_select "title", "Home | Ruby on Rails Tutorial"
+        assert_select "title", "Nicks Poor Website"
       end
 
     test "should get home" do
         get static_pages_home_url
         assert_response :success
-        assert_select "title", "Home | Ruby on Rails Tutorial"
+        assert_select "title", "Nicks Poor Website"
     end
 
     test "should get help" do
         get static_pages_help_url
         assert_response :success
-        assert_select "title", "Help | Ruby on Rails Tutorial"
+        assert_select "title", "Help | Nicks Poor Website"
     end
 
     test "should get about" do
         get static_pages_about_url
         assert_response :success
-        assert_select "title", "About | Ruby on Rails Tutorial"
+        assert_select "title", "About | Nicks Poor Website"
     end
 
 end
